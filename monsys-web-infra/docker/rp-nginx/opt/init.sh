e708503540e4b9177e2e60c4ffe12fe6bcac06bd
Olivier Liechti <olivier.liechti@lotaris.com>
2014-05-12 14:30:17 +0200

    Touching init.sh to fix end-of-line issue

--- monsys-web-infra/docker/rp-nginx/opt/init.sh
+++ monsys-web-infra/docker/rp-nginx/opt/init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Touched to fix the end-of-line problem
 # We would need to link the docker containers, but haven't done yet. Instead, we
 # rely on the fact that containers always have the same IP address. We use these
 # IP addresses in the nginx reverse proxy configuration. This is fragile and needs
